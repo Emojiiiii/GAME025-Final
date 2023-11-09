5033722b1ddf8d4171a3540e1079092b25388a34
Chaos-Inkarnate <mburdin@clarku.edu>
2023-11-09 15:51:36 -0500

    Update Movement.cs
    Basic Movement Script

--- Movement.cs
+++ Movement.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D player;
+    public float speed = 10.0f;
+    public bool flipX;
+    private SpriteRenderer playerFlip;
 
     void Start()
     {
-        
+        player = GetComponent<Rigidbody2D>();
+        playerFlip = GetComponent<SpriteRenderer>();
     }
 
-
-    
+    // Update is called once per frame
+    void Update()
+    { 
+            // adds force to move player sprite
+            player.AddForce(Input.GetAxis("Horizontal") * speed * transform.right);
+    }
 }
